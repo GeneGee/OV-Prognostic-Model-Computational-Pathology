7bb25250c3bdcafcf59b2a3b928a754825bad3ed
ZhangZhao <38484086+GeneGee@users.noreply.github.com>
2024-08-25 15:46:24 +0800

    Create extract.diagnostic.svs.py

---
+++ extract.diagnostic.svs.py
@@ -0,0 +1,18 @@
+import sys
+
+all_diagnostic_svs_manifest_file = sys.argv[1]
+all_ov_manifest_file = sys.argv[2]
+ov_diagnostic_svs_manifest_file = sys.argv[3]
+
+fw = open(ov_diagnostic_svs_manifest_file, "w")
+diag_svs_list = []
+with open(all_diagnostic_svs_manifest_file) as fr:
+  for line in fr.readlines():
+    recs = line.strip().split("\t")
+    diag_svs_list.append(recs)
+with open(all_ov_manifest_file) as fr:
+  for line in fr.readlines():
+    recs = line.strip().split("\t")
+    if recs in diag_svs_list:
+      fw.write("\t".join(recs)+"\n")
+  
