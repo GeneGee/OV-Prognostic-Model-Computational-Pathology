0a881bf9946232e6a540a5e2f95c97d4fc164636
ZhangZhao <38484086+GeneGee@users.noreply.github.com>
2024-08-29 11:58:24 +0800

    Update extract.info.svs.py

--- extract.info.svs.py
+++ extract.info.svs.py
@@ -8,6 +8,7 @@ import argparse
 # set svs directory path
 parser = argparse.ArgumentParser(description='Configurations for WSIs info extraction')
 parser.add_argument('--svs_dir', type=str, default=None, help='WSIs directory')
+parser.add_argument('--new_svs_dir', type=str, default=None, help='new WSIs directory')
 parser.add_argument('--info_file', type=str, default=None, help='output file stored WSIs info')
 args = parser.parse_args()
 
@@ -31,23 +32,26 @@ def extract_wsi_info(wsi_path):
   #  properties.append([key, slide.properties[key]])
   #  print([key, slide.properties[key]])
   #info_list.append(properties)
-  #magnification = 5
-  #downsample = level_0_magnification/5
-  #level = slide.get_best_level_for_downsample(downsample)
-  #image = slide.read_region((0,0), slide.level_count-5, slide.level_dimensions[5])
-  #image = cv2.cvtColor(np.array(image), cv2.COLOR_RGBA2BGR)
+  magnification = 20
+  downsample = level_0_magnification/magnification
+  level = slide.get_best_level_for_downsample(downsample)
+  image = slide.read_region((0,0), level, slide.level_dimensions[level])
+  image = cv2.cvtColor(np.array(image), cv2.COLOR_RGBA2BGR)
   #cv2.imwrite(output_path, image)
-  return info_list
+  return info_list, image
 
 def main():
   fw = open(args.info_file, "w")
   fw.write("\t".join(["Width","Height","Levels","Dimensions","Level_0_magnification"])+"\n")
   for file in os.listdir(args.svs_dir):
+    sample = '.'.join(file.split('.')[:-1])
     file_path = os.path.join(args.svs_dir, file)
     if not file_path.endswith('.svs'):
       continue
-    info_recs = extract_wsi_info(file_path)
+    info_recs, image = extract_wsi_info(file_path)
     fw.write("\t".join([str(rec) for rec in info_recs])+"\n")
+    new_image_path = os.path.join(args.new_svs_dir,sample+".tiff")
+    cv2.imwrite(new_image_path, image)
   fw.close()
   
 if __name__ == "__main__":
