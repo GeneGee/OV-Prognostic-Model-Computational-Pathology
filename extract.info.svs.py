c12956397ea3496c463989715503930cdd976163
ZhangZhao <38484086+GeneGee@users.noreply.github.com>
2024-09-05 13:50:35 +0800

    Update extract.info.svs.py

--- extract.info.svs.py
+++ extract.info.svs.py
@@ -3,21 +3,23 @@ import openslide
 import numpy as np
 import pandas as pd
 import cv2
+import pyvips
 import argparse
 
 # set svs directory path
 parser = argparse.ArgumentParser(description='Configurations for WSIs info extraction')
 parser.add_argument('--magnification', type=float, default=20.0, help='Magnification')
 parser.add_argument('--svs_dir', type=str, default=None, help='WSIs directory')
-parser.add_argument('--new_svs_dir', type=str, default=None, help='new WSIs directory')
+parser.add_argument('--new_dir', type=str, default=None, help='new WSIs directory')
 parser.add_argument('--info_file', type=str, default=None, help='output file stored WSIs info')
 args = parser.parse_args()
 
-def extract_wsi_info(wsi_path):
+def extract_wsi_info(wsi_path, new_path):
   slide = openslide.open_slide(wsi_path)
   width, height = slide.dimensions
   num_levels = slide.level_count
   dimensions = slide.level_dimensions
+  downsamples = slide.level_downsamples
   if 'aperio.AppMag' in slide.properties.keys():
     level_0_magnification = int(slide.properties['aperio.AppMag'])
     print('aperio.AppMag')
@@ -27,32 +29,25 @@ def extract_wsi_info(wsi_path):
   else:
     level_0_magnification = 40
     print('default')
-  info_list = [width, height, num_levels, dimensions, level_0_magnification]
-  #properties = []
-  #for key in slide.properties.keys():
-  #  properties.append([key, slide.properties[key]])
-  #  print([key, slide.properties[key]])
-  #info_list.append(properties)
+  info_list = [width, height, num_levels, downsamples, dimensions, level_0_magnification]
   magnification = args.magnification
-  downsample = level_0_magnification/magnification
-  level = slide.get_best_level_for_downsample(downsample)
-  image = slide.read_region((0,0), level, slide.level_dimensions[level])
-  image = cv2.cvtColor(np.array(image), cv2.COLOR_RGBA2BGR)
-  #cv2.imwrite(output_path, image)
-  return info_list, image
+  downsample = float(magnification)/level_0_magnification
+  new_image = pyvips.Image.new_from_file(wsi_path)
+  resize_image = new_image.resize(downsample)
+  resize_image.wirte_to_file(new_path)
+  return info_list
 
 def main():
   fw = open(args.info_file, "w")
-  fw.write("\t".join(["Width","Height","Levels","Dimensions","Level_0_magnification"])+"\n")
+  fw.write("\t".join(["Width","Height","Levels","Downsamples","Dimensions","Level_0_magnification"])+"\n")
   for file in os.listdir(args.svs_dir):
     sample = '.'.join(file.split('.')[:-1])
     file_path = os.path.join(args.svs_dir, file)
+    new_file_path = os.path.join(args.new_dir, sample+".tiff")
     if not file_path.endswith('.svs'):
       continue
-    info_recs, image = extract_wsi_info(file_path)
+    info_recs = extract_wsi_info(file_path, new_file_path)
     fw.write("\t".join([str(rec) for rec in info_recs])+"\n")
-    new_image_path = os.path.join(args.new_svs_dir,sample+".jpg")
-    cv2.imwrite(new_image_path, image)
   fw.close()
   
 if __name__ == "__main__":
